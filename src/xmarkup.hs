108220a91d7e95ca16c41c5e7bfd1d694f8e33cf
metaleap <philipp.schumann@gmail.com>
2016-12-18 22:15:29 +0100

    ~

--- src/xmarkup.hs
+++ src/xmarkup.hs
@@ -3,8 +3,13 @@ module XMarkup where
 import Pages
 
 
-ext tagname fulltag = Pages.X [ Pages.Tmpl tagname apply ] where
-    -- could also do:
-    -- Html.out fulltag [("",argstr)] []
-    -- but let's keep this as a lean template showcasing the simplicity of custom-coded X-tag providers
+--	simply rewrites occurences such as `{X{C: foo}}` or `{X{BQ: bar}}`
+--	out into markup such as `<code>foo</code>` or `<blockquote>bar</blockquote>`
+--	with one-off single-line definitions in `haxtatic.config` such as:
+--  `X:Markup:C:code` and `X:Markup:BQ:blockquote` etc.
+
+ext tname fulltag = Pages.X [ Pages.Tmpl tname apply ] where
     apply _ argstr _ = [ "<"++fulltag++">"++argstr++"</"++fulltag++">" ]
+    --	could also do instead:
+    --		Html.out fulltag [("",argstr)] []
+    --	but let's keep this as a lean template showcasing the simplicity of custom-coded X-tag providers
