e5f09b50c91277c855d8769900b0c35e473f1d55
metaleap <philipp.schumann@gmail.com>
2017-01-16 15:09:12 +0100

    refr

--- __tmp_nu_/misc/haxdox.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem to run from haxtatic/src/main.hs' folder
-
-del /F /Q /S ..\docs\*
-del /F /Q /S ..\src-docs\build\*
-stack runghc main.hs ..\src-docs
-xcopy ..\src-docs\build\* ..\docs\ /S /I /Q /Y
