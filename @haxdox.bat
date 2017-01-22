b7b62c1a349bfbbbcb0a7e3f5edd3011d83f35af
metaleap <philipp.schumann@gmail.com>
2017-01-22 23:56:58 +0100

    lnk

--- @haxdox.bat
+++ @haxdox.bat
@@ -1,8 +1,7 @@
 @echo off
-rem to run from haxtatic/src folder
+rem TO RUN ONLY FROM haxtatic/src folder!!
 
-rem del /F /Q /S ..\dox-demo\build\*
+rem del /F /Q /S ..\dox-demo\default-build\*
 rem del /F /Q /S ..\docs\*
 stack .\@HaxMain.hs ..\dox-demo
-rem doxdemo.haxproj overwrites.haxproj posts.haxproj
-rem xcopy ..\dox-demo\build\* ..\docs\ /S /I /Q /Y
+rem xcopy ..\dox-demo\default-build\* ..\docs\ /S /I /Q /Y
