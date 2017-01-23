b517507b4725d16d28aeb393488cefd790affc50
metaleap <philipp.schumann@gmail.com>
2017-01-23 15:03:18 +0100

    cleanup outdated ver

--- __tmp_nu_/@haxdox.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem to run from haxtatic/src folder
-
-rem del /F /Q /S ..\dox-demo\build\*
-rem del /F /Q /S ..\docs\*
-stack .\@HaxMain.hs ..\dox-demo
-rem doxdemo.haxproj overwrites.haxproj posts.haxproj
-rem xcopy ..\dox-demo\build\* ..\docs\ /S /I /Q /Y
